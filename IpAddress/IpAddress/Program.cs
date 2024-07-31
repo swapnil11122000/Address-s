fb6b020285a4be82ca81b30b365d1f714b9ba953
swapnil Muluk <111170645+swapnil11122000@users.noreply.github.com>
2024-07-31 23:28:44 +0530

    Add files via upload

---
+++ IpAddress/IpAddress/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.NetworkInformation;
+
+namespace IpAddress
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string ipAddress = "";
+            IPHostEntry host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (IPAddress ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    ipAddress = ip.ToString();
+                    break;
+                }
+            }
+            Console.WriteLine("IP Address: " + ipAddress);
+
+
+
+            string macAddress = "";
+            foreach (NetworkInterface nic in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                if (nic.OperationalStatus == OperationalStatus.Up && nic.NetworkInterfaceType != NetworkInterfaceType.Loopback)
+                {
+                    macAddress = nic.GetPhysicalAddress().ToString();
+                    break;
+                }
+            }
+            Console.WriteLine("MAC Address: " + macAddress);
+
+        }
+    }
+}
